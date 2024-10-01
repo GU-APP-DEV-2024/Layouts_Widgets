4df5fc59afca64cbfa78d7c2904e09997c0b6ec5
Harley Davis <asphixiatedaphid@gmail.com>
2024-10-01 10:27:03 -0700

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import memorygame.Board
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var card1: ImageView
+    private lateinit var card2: ImageView
+    private lateinit var card3: ImageView
+    private lateinit var card4: ImageView
+    private lateinit var card5: ImageView
+    private lateinit var card6: ImageView
+
+    private lateinit var board: Board
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        board = Board()
+
+
+
+        card1 = findViewById(R.id.card_1)
+        card2 = findViewById(R.id.card_2)
+        card3 = findViewById(R.id.card_3)
+        card4 = findViewById(R.id.card_4)
+        card5 = findViewById(R.id.card_5)
+        card6 = findViewById(R.id.card_6)
+    }
+
+    fun getCardID(view: View): Int {
+        when (view) {
+            card1 -> return 0
+            card2 -> return 1
+            card3 -> return 2
+            card4 -> return 3
+            card5 -> return 4
+            else -> return 5
+        }
+    }
+
+    fun onClick(view: View) {
+        view as ImageView
+        var card =  board.getValueAt(getCardID(view))
+
+        view.setImageResource(getImageID(card))
+    }
+
+    fun getImageID(card: Board.Card) : Int {
+        when (card) {
+            Board.Card('c', "a") -> return R.drawable.card_clubs_a
+            Board.Card('h', "10") -> return R.drawable.card_hearts_10
+            Board.Card('d', "3") -> return R.drawable.card_diamonds_3
+            else -> return R.drawable.card_back_green
+        }
+    }
+}
